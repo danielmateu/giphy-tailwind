a07e59377766812b728a1f7abb952b31f15bc351
Daniel Mateu <danielmateu86@gmail.com>
2023-02-22 16:34:35 +0100

    Share buttons added

--- src/components/GIfItem.jsx
+++ src/components/GIfItem.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { FaFacebook, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+
 
 export const GIfItem = ({ url, title }) => {
-    
+
+    const shareUrl = encodeURI(url);
+
+
+    const shareOnFacebook = () => {
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}&t=${title}`);
+    };
+
+    const shareOnTwitter = () => {
+        window.open(`https://twitter.com/intent/tweet?url=${shareUrl}&text=${title}`);
+    };
+
+    const shareOnWhatsapp = () => {
+        window.open(`https://api.whatsapp.com/send?text=${shareUrl}`);
+    };
+
+
     return (
         <div
             className='p-2  rounded-md shadow-md  hover:shadow-none hover:border-gray-500 bg-white transition-all flex flex-col justify-between'
@@ -11,6 +28,18 @@ export const GIfItem = ({ url, title }) => {
                 alt={title}
             />
             <a href={url} target='_blank' className='hover:text-gray-500'>{title}</a>
+            <div className='flex gap-4 justify-end items-center'>
+                <p>Quieres compartir?</p>
+                <button 
+                    className='hover:text-gray-400 hover:-rotate-45 transition'
+                    onClick={shareOnFacebook}><FaFacebook /></button>
+                <button 
+                    className='hover:text-gray-400 hover:-rotate-45 transition'
+                    onClick={shareOnTwitter}><FaTwitter /></button>
+                <button 
+                    className='hover:text-gray-400 hover:-rotate-45 transition'
+                    onClick={shareOnWhatsapp}><FaWhatsapp /></button>
+            </div>
         </div>
     )
 }
