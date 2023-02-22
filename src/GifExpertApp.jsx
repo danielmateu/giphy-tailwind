297035485850cf56feda43569edca0c4e6435eec
Daniel Mateu <danielmateu86@gmail.com>
2023-02-22 10:18:49 +0100

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+
+    const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+
+
+    return (
+        <div className='bg-sky-400 p-10'>
+            <h1 className='text-4xl text-center'>GifExpertApp</h1>
+        </div>
+    )
+}
